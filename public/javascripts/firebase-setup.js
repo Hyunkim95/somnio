e4810207c81a9b2b1670f06159639f65fe41f154
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-11-17 07:23:28 -0800

    add cars

--- public/javascripts/firebase-setup.js
+++ public/javascripts/firebase-setup.js
@@ -59,7 +59,9 @@ var SceneVue = new Vue({
           ["modern-convertible-sofa-with-pullout-bed", "Bed"],
           ["wooden-coffe-table", "Wooden Coffee Table"],
           ["lamp", "Lamp"],
-          ["ferrari-laferrari", "Ferrari"]
+          ["ferrari-laferrari", "Ferrari"],
+          ["nissan-gt-r-nismo", "Nissan GT-R"],
+          ["lamborghini-aventador-irridescent-paint", "Laborghini"]
         ]
     },
     methods: {
